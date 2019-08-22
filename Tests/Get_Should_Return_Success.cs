98e799696be6ae5760e5c798b7d87a744ea8717e
sashaby <aleks.bykova@gmail.com>
2019-08-22 13:40:59 -0700

    adding webapi files to the repository

---
+++ Tests/Get_Should_Return_Success.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using System.Net.Http;
+
+namespace Tests
+{
+    public class IntegrationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+           // var httpclient = new HttpClient();
+        }
+
+        [Test]
+        public void Get_Should_Return_Success()
+        {
+            var httpclient = new HttpClient();
+            httpclient.BaseAddress = new System.Uri("https://localhost:44374/api/");
+            var responseTask = httpclient.GetAsync("users");
+            responseTask.Wait();
+            var result = responseTask.Result;
+                       
+            Assert.IsTrue(result.IsSuccessStatusCode);
+        }
+    }
+}
